870c204b6daf96373a258b843ac1e9922133173d
stefan <inis.projekt.pharma@gmail.com>
2021-01-11 17:17:37 +0100

    layouts verbessert teilweise Camaraschon hinzugefügt

--- app/src/main/java/com/example/android/persistence/ui/kontakt_erstellen.java
+++ app/src/main/java/com/example/android/persistence/ui/kontakt_erstellen.java
@@ -1,24 +1,71 @@
 package com.example.android.persistence.ui;
 //Heger Stefan
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.android.persistence.R;
 import com.example.android.persistence.db.entity.AdressEntity;
 import com.example.android.persistence.db.entity.InteressentEntity;
 import com.example.android.persistence.db.entity.PersonEntity;
+import com.example.android.persistence.model.Interessent;
 import com.example.android.persistence.viewmodel.kontakt_erstellenViewModel;
 
-public class kontakt_erstellen extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class kontakt_erstellen extends AppCompatActivity {
+    private static final int PERMISSION_CODE = 1000 ;
+    private static final int IMAGE_CAPTURE_CODE = 1001 ;
+    kontakt_erstellenViewModel keVM=new kontakt_erstellenViewModel(getApplication());
+
+    private static final int REQUEST_IMAGE_CAPTURE =1 ;
     private PersonEntity person;
     private InteressentEntity interessent;
     private AdressEntity adresse;
+
+
+    String currentPhotoPath;
+    Uri image_uri;
+
+
+    //PersonEntity
+    EditText name ;
+    EditText nachname ;
+    EditText firma ;
+    EditText alter ;
+    //InteresentEntity
+    EditText stellung;
+    //Adress Entity
+    EditText mTelNr ;
+    EditText telNr ;
+    EditText plz ;
+    EditText land ;
+    EditText ort ;
+    EditText strasse;
+    EditText hausNr ;
+    EditText notiz ;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,28 +93,73 @@ public class kontakt_erstellen extends AppCompatActivity {
         accept.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 scanData();
-                kontakt_erstellenViewModel.createInteressent(person,interessent,adresse);
+
+                keVM.createInteressent(person,interessent,adresse);
                 startActivity(new Intent(getApplicationContext(), kontakte.class));
             }
         });
+
+        Button scan=(Button)findViewById(R.id.scanCardButton);
+        scan.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+           if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+               if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                   String[] permission ={Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                   requestPermissions(permission, PERMISSION_CODE);
+               }
+               else{//darf schon resourcen benutzen
+                    openCamera();
+               }
+
+           }
+            else{
+                //alte VErsion
+               openCamera();
+           }
+
+            }
+        });
+
+
     }
+    private void openCamera(){
+        ContentValues values=new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE,"new vcard");
+        values.put(MediaStore.Images.Media.DESCRIPTION,"new vcard");
+        image_uri=getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+        Intent cameraIntent =new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,image_uri);
+        startActivityForResult(cameraIntent,IMAGE_CAPTURE_CODE);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+       switch(requestCode){
+           case PERMISSION_CODE:{
+               if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                   openCamera();
+               }
+               else{
+                   Toast.makeText(this,"Permission denied...",Toast.LENGTH_SHORT).show();
+               }
+
+           }
+       }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            Bundle extras =data.getExtras();
+            Bitmap imgBitmap= (Bitmap) extras.get("data");
+            keVM.getDataByImg(this,imgBitmap);
+        }
+    }
+
     private void scanData(){
-        //PersonEntity
-        EditText name = (EditText) findViewById(R.id.editTextFirstName);
-        EditText nachname = (EditText) findViewById(R.id.editTextFirstName);
-        EditText firma = (EditText) findViewById(R.id.editTextCompany);
-        EditText alter = (EditText) findViewById(R.id.editTextAge);
-        //InteresentEntity
-        EditText stellung = (EditText) findViewById(R.id.editTextStellung);
-        //Adress Entity
-        EditText mTelNr = (EditText) findViewById(R.id.editTextMobile);
-        EditText telNr = (EditText) findViewById(R.id.editTextPhone);
-        EditText plz = (EditText) findViewById(R.id.editTextPLZ);
-        EditText land = (EditText) findViewById(R.id.editTextLand);
-        EditText ort = (EditText) findViewById(R.id.editTextOrt);
-        EditText strasse = (EditText) findViewById(R.id.editTextStrasse);
-        EditText hausNr = (EditText) findViewById(R.id.editTextHausnummer);
-        EditText notiz = (EditText) findViewById(R.id.editTextNotiz);
+
         int sAlter =Integer.parseInt(alter.getText().toString());
 
         String sName=name.getText().toString();
@@ -91,6 +183,47 @@ public class kontakt_erstellen extends AppCompatActivity {
 
 
 
+    }
+    public void setData(PersonEntity pe, InteressentEntity ie, AdressEntity ae){
+        this.person=pe;
+        this.interessent=ie;
+        this.adresse=ae;
+        writeDataToView( pe,  ie,  ae);
+    }
+    private void writeDataToView(PersonEntity pe, InteressentEntity ie, AdressEntity ae){
+        //PersonEntity
+        name.setText(pe.getName());
+       nachname.setText(pe.getNachname());
+        EditText firma = (EditText) findViewById(R.id.editTextCompany);
+        alter.setText(pe.getAlter());
+        //InteresentEntity
+        stellung.setText(ie.getStellung());
+        //Adress Entity
+        mTelNr.setText(ae.getmTelNr());
+        telNr.setText(ae.getTelNr());
+        plz.setText(ae.getPlz());
+        land.setText(ae.getLand());
+        ort.setText(ae.getOrt());
+        strasse.setText(ae.getStrasse());
+        hausNr.setText(ae.getHausnummer());
+        notiz.setText(ie.getNotiz());
+    }
+    private void initTextField(){
+         name = (EditText) findViewById(R.id.editTextFirstName);
+         nachname = (EditText) findViewById(R.id.editTextFirstName);
+         firma = (EditText) findViewById(R.id.editTextCompany);
+         alter = (EditText) findViewById(R.id.editTextAge);
+        //InteresentEntity
+         stellung = (EditText) findViewById(R.id.editTextStellung);
+        //Adress Entity
+         mTelNr = (EditText) findViewById(R.id.editTextMobile);
+         telNr = (EditText) findViewById(R.id.editTextPhone);
+         plz = (EditText) findViewById(R.id.editTextPLZ);
+         land = (EditText) findViewById(R.id.editTextLand);
+         ort = (EditText) findViewById(R.id.editTextOrt);
+         strasse = (EditText) findViewById(R.id.editTextStrasse);
+         hausNr = (EditText) findViewById(R.id.editTextHausnummer);
+         notiz = (EditText) findViewById(R.id.editTextNotiz);
     }
 
 }
--- app/src/main/java/com/example/android/persistence/viewmodel/kontakt_erstellenViewModel.java
+++ app/src/main/java/com/example/android/persistence/viewmodel/kontakt_erstellenViewModel.java
@@ -1,19 +1,48 @@
 package com.example.android.persistence.viewmodel;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
 import androidx.lifecycle.AndroidViewModel;
 
 import com.example.android.persistence.db.entity.AdressEntity;
 import com.example.android.persistence.db.entity.InteressentEntity;
 import com.example.android.persistence.db.entity.PersonEntity;
+import com.example.android.persistence.ui.kontakt_erstellen;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static androidx.core.app.ActivityCompat.startActivityForResult;
 
 public class kontakt_erstellenViewModel extends AndroidViewModel {
+    PersonEntity pe; InteressentEntity ie; AdressEntity ae;
+    String currentPhotoPath;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
     public kontakt_erstellenViewModel(@NonNull Application application) {
         super(application);
     }
-    public static void createInteressent(PersonEntity pe, InteressentEntity ie, AdressEntity ae){
+    public void createInteressent(PersonEntity pe, InteressentEntity ie, AdressEntity ae){
+    this.pe=pe;
+    this.ie=ie;
+    this.ae=ae;
+    }
+    public void getDataByImg(kontakt_erstellen ke, Bitmap imgBitmap){
+
 
     }
+
+
+
 }
+
