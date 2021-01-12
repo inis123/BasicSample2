218c7e342f8817332e60bbed95702de6fcca9fe7
stefan <inis.projekt.pharma@gmail.com>
2021-01-12 19:46:34 +0100

    http für visitenkarten

--- app/src/main/java/com/example/android/persistence/connection/vcard.java
+++ app/src/main/java/com/example/android/persistence/connection/vcard.java
@@ -3,7 +3,7 @@ package com.example.android.persistence.connection;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.util.Log;
+
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -11,32 +11,22 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.android.persistence.db.entity.AdressEntity;
+import com.example.android.persistence.db.entity.InteressentEntity;
+import com.example.android.persistence.db.entity.PersonEntity;
 import com.example.android.persistence.ui.kontakt_erstellen;
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
+
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
+
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
-
-
-import javax.net.ssl.HttpsURLConnection;
-
-import cz.msebera.android.httpclient.entity.mime.Header;
 
 public class vcard {
     private kontakt_erstellen ke;
@@ -48,23 +38,27 @@ public class vcard {
     }
 
 
-    public void getRequest(Context context){
-        boolean faild =false;
+    public void getRequest(Context context) {
 
-       RequestQueue mQueue= Volley.newRequestQueue(context);
-       String url="";
-        JsonObjectRequest request =new JsonObjectRequest(Request.Method.GET, url, null,
+
+        RequestQueue mQueue = Volley.newRequestQueue(context);
+        String url = "https://{endpoint}/formrecognizer/v2.1-preview.2/prebuilt/businessCard/analyzeResults/{resultId}";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
 
                         try {
-                            JSONObject json=response.getJSONObject("status");
-                            if(json.toString().contains("failed"))
+                            JSONObject json = response.getJSONObject("status");
+                            if (json.toString().contains("failed"))
                                 return;
-                            if(json.toString().contains("succeeded"))
+                            if (json.toString().contains("succeeded")) {
                                 setData(response);
                                 return;
+                            }
+                            if (json.toString().contains("running") || json.toString().contains("notStarted"))
+                                getRequest(context);                                                        // Wiederholverzögerung oder als eigener Task
+                            return;
 
 
                         } catch (JSONException e) {
@@ -82,9 +76,7 @@ public class vcard {
     }
 
 
-
-
-    public void  performPostCall(Bitmap img){
+    public void performPostCall(Bitmap img) {
         int width = img.getWidth();
         int height = img.getHeight();
 
@@ -92,8 +84,7 @@ public class vcard {
         ByteBuffer byteBuffer = ByteBuffer.allocate(size);
         img.copyPixelsToBuffer(byteBuffer);
         byte[] byteArray = byteBuffer.array();
-        try
-        {
+        try {
 
             // Defined URL  where to send data
             URL url = new URL("https://northeurope.api.cognitive.microsoft.com/formrecognizer/v2.1-preview.2/prebuilt/businessCard/analyze");
@@ -101,23 +92,45 @@ public class vcard {
             // Send POST data request
 
             URLConnection conn = url.openConnection();
-            conn.setRequestProperty("Content-Type","image/jpeg");
+            conn.setRequestProperty("Content-Type", "image/jpeg");
             conn.setDoOutput(true);
-            OutputStream os=conn.getOutputStream();
+            OutputStream os = conn.getOutputStream();
             os.write(byteArray);
             os.flush();
             os.close();
             conn.connect();
 
 
-        }
-        catch(Exception ex)
-        {
+        } catch (Exception ex) {
 
         }
 
     }
-    public void setData(JSONObject response){
+
+    public void setData(JSONObject response) {
+        PersonEntity pe = new PersonEntity();
+        AdressEntity ae = new AdressEntity();
+        InteressentEntity ie = new InteressentEntity();
+
+        try {
+            JSONArray jarr = response.getJSONObject("analyzeResult").getJSONArray("documentResults");
+            JSONObject tmp = jarr.getJSONObject(0).getJSONObject("fields").getJSONObject("ContactNames").getJSONObject("valueObject");
+            JSONObject fields = jarr.getJSONObject(0).getJSONObject("fields");
+
+
+            pe.setName(tmp.getJSONObject("FirstName").getString("text"));
+            pe.setNachname(tmp.getJSONObject("LastName").getString("text"));
+            ie.setStellung(fields.getJSONObject("JobTitles").getJSONArray("valueArray").getJSONObject(0).getString("text"));
+
+            ae.setmTelNr(fields.getJSONObject("OtherPhones").getJSONArray("valueArray").getJSONObject(0).getString("text"));
+            ae.setTelNr(fields.getJSONObject("MobilPhones").getJSONArray("valueArray").getJSONObject(0).getString("text"));
+            ae.setOrt(fields.getJSONObject("Adresses").getJSONArray("valueArray").getJSONObject(0).getString("text"));
+
+
+        } catch (Exception a) {
+
+        }
+        ke.setData(pe, ie, ae);
 
     }
 
--- app/src/main/java/com/example/android/persistence/ui/kontakt_erstellen.java
+++ app/src/main/java/com/example/android/persistence/ui/kontakt_erstellen.java
@@ -1,5 +1,6 @@
 package com.example.android.persistence.ui;
 //Heger Stefan
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -34,36 +35,36 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class kontakt_erstellen extends AppCompatActivity {
-    private static final int PERMISSION_CODE = 1000 ;
-    private static final int IMAGE_CAPTURE_CODE = 1001 ;
-    kontakt_erstellenViewModel keVM=new kontakt_erstellenViewModel(getApplication());
+    private static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_CAPTURE_CODE = 1001;
+    kontakt_erstellenViewModel keVM = new kontakt_erstellenViewModel(getApplication());
 
-    private static final int REQUEST_IMAGE_CAPTURE =1 ;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
     private PersonEntity person;
     private InteressentEntity interessent;
     private AdressEntity adresse;
 
 
-    String currentPhotoPath;
-    Uri image_uri;
+    private String currentPhotoPath;
+    private Uri image_uri;
 
 
     //PersonEntity
-    EditText name ;
-    EditText nachname ;
-    EditText firma ;
-    EditText alter ;
+    private EditText name;
+    private EditText nachname;
+    private EditText firma;
+    private EditText alter;
     //InteresentEntity
-    EditText stellung;
+    private EditText stellung;
     //Adress Entity
-    EditText mTelNr ;
-    EditText telNr ;
-    EditText plz ;
-    EditText land ;
-    EditText ort ;
-    EditText strasse;
-    EditText hausNr ;
-    EditText notiz ;
+    private EditText mTelNr;
+    private EditText telNr;
+    private EditText plz;
+    private EditText land;
+    private EditText ort;
+    private EditText strasse;
+    private EditText hausNr;
+    private EditText notiz;
 
 
     @Override
@@ -71,129 +72,130 @@ public class kontakt_erstellen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kontakt_erstellen);
 
-        navBar.initNavBar(getApplicationContext(),this);
+        navBar.initNavBar(getApplicationContext(), this);
 
         setListener();
 
 
-
-
-
     }
-    private void setListener(){
+
+    private void setListener() {
         ImageButton abort = (ImageButton) findViewById(R.id.abortButton);
-        abort.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        abort.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), home.class));
             }
         });
 
 
-        ImageButton accept=(ImageButton)findViewById(R.id.acceptButton);
-        accept.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        ImageButton accept = (ImageButton) findViewById(R.id.acceptButton);
+        accept.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 scanData();
 
-                keVM.createInteressent(getApplicationContext(),person,interessent,adresse);
+                keVM.createInteressent(getApplicationContext(), person, interessent, adresse);
                 startActivity(new Intent(getApplicationContext(), kontakte.class));
             }
         });
 
-        Button scan=(Button)findViewById(R.id.scanCardButton);
-        scan.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-           if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
-               if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
-                   String[] permission ={Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE};
-                   requestPermissions(permission, PERMISSION_CODE);
-               }
-               else{//darf schon resourcen benutzen
-                    openCamera();
-               }
+        Button scan = (Button) findViewById(R.id.scanCardButton);
+        scan.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        requestPermissions(permission, PERMISSION_CODE);
+                    } else {//darf schon resourcen benutzen
+                        openCamera();
+                    }
 
-           }
-            else{
-                //alte VErsion
-               openCamera();
-           }
+                } else {
+                    //alte VErsion
+                    openCamera();
+                }
 
             }
         });
 
 
     }
-    private void openCamera(){
-        ContentValues values=new ContentValues();
-        values.put(MediaStore.Images.Media.TITLE,"new vcard");
-        values.put(MediaStore.Images.Media.DESCRIPTION,"new vcard");
-        image_uri=getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
-        Intent cameraIntent =new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,image_uri);
-        startActivityForResult(cameraIntent,IMAGE_CAPTURE_CODE);
+
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "new vcard");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "new vcard");
+
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
 
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-       switch(requestCode){
-           case PERMISSION_CODE:{
-               if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                   openCamera();
-               }
-               else{
-                   Toast.makeText(this,"Permission denied...",Toast.LENGTH_SHORT).show();
-               }
+        switch (requestCode) {
+            case PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openCamera();
+                } else {
+                    Toast.makeText(this, "Permission denied...", Toast.LENGTH_SHORT).show();
+                }
 
-           }
-       }
+            }
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
-            Bundle extras =data.getExtras();
-            Bitmap imgBitmap= (Bitmap) extras.get("data");
-            keVM.getDataByImg(this,imgBitmap);
+
+            Bundle extras = data.getExtras();
+
+            Bitmap imgBitmap = (Bitmap) extras.get("data");
+            keVM.getDataByImg(this, imgBitmap);
         }
     }
 
-    private void scanData(){
+    private void scanData() {
 
-        int sAlter =Integer.parseInt(alter.getText().toString());
+        int sAlter = Integer.parseInt(alter.getText().toString());
 
-        String sName=name.getText().toString();
-        String sNachname=nachname.getText().toString();
-        String sFirma=firma.getText().toString();
-        String sStellung=stellung.getText().toString();
-        String sMTelNr=mTelNr.getText().toString();
-        String sTelNr=telNr.getText().toString();
-        String sPlz=plz.getText().toString();
-        String sLand=land.getText().toString();
-        String sOrt=ort.getText().toString();
-        String sStrasse=strasse.getText().toString();
-        String sHausNr=hausNr.getText().toString();
-        String sNotiz=notiz.getText().toString();
+        String sName = name.getText().toString();
+        String sNachname = nachname.getText().toString();
+        String sFirma = firma.getText().toString();
+        String sStellung = stellung.getText().toString();
+        String sMTelNr = mTelNr.getText().toString();
+        String sTelNr = telNr.getText().toString();
+        String sPlz = plz.getText().toString();
+        String sLand = land.getText().toString();
+        String sOrt = ort.getText().toString();
+        String sStrasse = strasse.getText().toString();
+        String sHausNr = hausNr.getText().toString();
+        String sNotiz = notiz.getText().toString();
 
 
-
-        person = new PersonEntity(sName,sNachname,sAlter);
-        interessent = new InteressentEntity(sNotiz,sStellung);
-        adresse = new AdressEntity(sOrt,sStrasse,sHausNr,sLand,sPlz,sMTelNr,sTelNr);
-
+        person = new PersonEntity(sName, sNachname, sAlter);
+        interessent = new InteressentEntity(sNotiz, sStellung);
+        adresse = new AdressEntity(sOrt, sStrasse, sHausNr, sLand, sPlz, sMTelNr, sTelNr);
 
 
     }
-    public void setData(PersonEntity pe, InteressentEntity ie, AdressEntity ae){
-        this.person=pe;
-        this.interessent=ie;
-        this.adresse=ae;
-        writeDataToView( pe,  ie,  ae);
+
+    public void setData(PersonEntity pe, InteressentEntity ie, AdressEntity ae) {
+        this.person = pe;
+        this.interessent = ie;
+        this.adresse = ae;
+        writeDataToView(pe, ie, ae);
     }
-    private void writeDataToView(PersonEntity pe, InteressentEntity ie, AdressEntity ae){
+
+    private void writeDataToView(PersonEntity pe, InteressentEntity ie, AdressEntity ae) {
         //PersonEntity
         name.setText(pe.getName());
-       nachname.setText(pe.getNachname());
+        nachname.setText(pe.getNachname());
         EditText firma = (EditText) findViewById(R.id.editTextCompany);
         alter.setText(pe.getAlter());
         //InteresentEntity
@@ -208,22 +210,23 @@ public class kontakt_erstellen extends AppCompatActivity {
         hausNr.setText(ae.getHausnummer());
         notiz.setText(ie.getNotiz());
     }
-    private void initTextField(){
-         name = (EditText) findViewById(R.id.editTextFirstName);
-         nachname = (EditText) findViewById(R.id.editTextFirstName);
-         firma = (EditText) findViewById(R.id.editTextCompany);
-         alter = (EditText) findViewById(R.id.editTextAge);
+
+    private void initTextField() {
+        name = (EditText) findViewById(R.id.editTextFirstName);
+        nachname = (EditText) findViewById(R.id.editTextFirstName);
+        firma = (EditText) findViewById(R.id.editTextCompany);
+        alter = (EditText) findViewById(R.id.editTextAge);
         //InteresentEntity
-         stellung = (EditText) findViewById(R.id.editTextStellung);
+        stellung = (EditText) findViewById(R.id.editTextStellung);
         //Adress Entity
-         mTelNr = (EditText) findViewById(R.id.editTextMobile);
-         telNr = (EditText) findViewById(R.id.editTextPhone);
-         plz = (EditText) findViewById(R.id.editTextPLZ);
-         land = (EditText) findViewById(R.id.editTextLand);
-         ort = (EditText) findViewById(R.id.editTextOrt);
-         strasse = (EditText) findViewById(R.id.editTextStrasse);
-         hausNr = (EditText) findViewById(R.id.editTextHausnummer);
-         notiz = (EditText) findViewById(R.id.editTextNotiz);
+        mTelNr = (EditText) findViewById(R.id.editTextMobile);
+        telNr = (EditText) findViewById(R.id.editTextPhone);
+        plz = (EditText) findViewById(R.id.editTextPLZ);
+        land = (EditText) findViewById(R.id.editTextLand);
+        ort = (EditText) findViewById(R.id.editTextOrt);
+        strasse = (EditText) findViewById(R.id.editTextStrasse);
+        hausNr = (EditText) findViewById(R.id.editTextHausnummer);
+        notiz = (EditText) findViewById(R.id.editTextNotiz);
     }
 
 }
