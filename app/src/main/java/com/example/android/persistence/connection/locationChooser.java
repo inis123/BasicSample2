a4833f917c68ccb280cecd5fdf24443d98aed554
stefan <inis.projekt.pharma@gmail.com>
2021-01-13 14:13:43 +0100

    Nachsten Terminfinder

--- app/src/main/java/com/example/android/persistence/connection/locationChooser.java
+++ app/src/main/java/com/example/android/persistence/connection/locationChooser.java
@@ -7,8 +7,7 @@ import com.example.android.persistence.db.entity.AdressEntity;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
-import java.util.Stack;
+
 
 public class locationChooser {
     public static AdressEntity choose(Context context,AdressEntity eigeneAdr){
@@ -17,18 +16,18 @@ public class locationChooser {
         AppDatabase db=AppDatabase.getInstance(context);
         List<AdressEntity> adr=db.adressDao().getAllAdress();
         geoRadius gr=new geoRadius(context,eigeneAdr);
-        AdressEntity choosen=adr.get(0);
+        AdressEntity ch=adr.get(0);
         double tmp=Double.MAX_VALUE;
         double c;
         for(int i=0;i<adr.size();i++){
              c=gr.getNewDistance(adr.get(i));
              di.add((int)c);
              if(c<tmp) {
-                 choosen = adr.get(i);
+                 ch = adr.get(i);
                  tmp=c;
              }
 
         }
-        return choosen;
+        return ch;
     }
 }
